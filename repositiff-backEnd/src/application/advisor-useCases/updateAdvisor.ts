81038c6bae3d9da473b12534da9f8de3ac1e8ddf
natanMoutinho <natan.nt.iff@gmail.com>
2024-11-30 10:10:17 -0300

    Feat: Update de orientador finaizado, falta escrever os testes

--- repositiff-backEnd/src/application/advisor-useCases/updateAdvisor.ts
+++ repositiff-backEnd/src/application/advisor-useCases/updateAdvisor.ts
@@ -1,9 +1,10 @@
 import { Advisor } from "@src/domain/advisor.js";
 import { AdvisorFactory } from "@src/domain/factories/advisorFactory.js";
 import { DomainError, ErrorCategory } from "@src/error_handling/domainServicesErrors.js";
+import { Either, Left, Right } from "@src/error_handling/either.js";
 import { IAdvisorRepository } from "@src/infra/repositories/IAdvisorRepository.js";
 
-export interface updateAdvisorPropsDTO {
+export interface UpdateAdvisorPropsDTO {
     advisorIdentification: string,
     updateFields: UpdateFieldsDTO
 }
@@ -20,14 +21,16 @@ export class UpdateAdvisorUseCase {
         private advisorRepository: IAdvisorRepository,
     ) { }
 
-    async execute(updateAdvisorProps: updateAdvisorPropsDTO): Promise<DomainError | Advisor> {
+    async execute(updateAdvisorProps: UpdateAdvisorPropsDTO): Promise<DomainError | Advisor> {
         const updateFields = updateAdvisorProps.updateFields;
 
         // filtra os valores que não são undfined, deixando apenas o que será atualizado
-        const advisorDefinedFieldsToUpdate = Object.entries(updateFields)
-            .filter(([key, value]) => value !== undefined) as [keyof updateAdvisorPropsDTO["updateFields"], string][]; // Garante que as chaves correspondem ao tipo UpdateFields
+        const advisorDefinedFields = Object.entries(updateFields)
+            .filter(([key, value]) => value !== undefined); // Garante que as chaves correspondem ao tipo UpdateFields
+        // .filter(([key, value]) => value !== undefined) as [keyof UpdateAdvisorPropsDTO["updateFields"], string][]; // Garante que as chaves correspondem ao tipo UpdateFields
+
         // Verificação para saber se todos os campos para upload não foram passados
-        if (advisorDefinedFieldsToUpdate.length === 0) {
+        if (advisorDefinedFields.length === 0) {
             return new DomainError(
                 ErrorCategory.Application,
                 "Error to update advisor attributes",
@@ -35,7 +38,7 @@ export class UpdateAdvisorUseCase {
             );
         }
         // Seleciona o orientador que terá seus dados atualizados
-        const advisorExisting = await this.advisorRepository.findAdvisorById(updateAdvisorProps.advisorIdentification);
+        const advisorExisting = await this.advisorRepository.advisorExisting(updateAdvisorProps.advisorIdentification);
         if (!advisorExisting) {
             return new DomainError(
                 ErrorCategory.Application,
@@ -44,84 +47,85 @@ export class UpdateAdvisorUseCase {
                     "Advisor not registrated"
                 ]);
         }
+        let validFields: UpdateFieldsDTO = {};
+        for (let i = 0; i < advisorDefinedFields.length; i++) {
+            const fieldUpdate = this.updateValidateFields(
+                advisorDefinedFields[i][0],// Pega a "chave" Ex: "name"
+                advisorDefinedFields[i][1] // Pega o "valor" Ex: "fulano" 
+            );
 
-        advisorDefinedFieldsToUpdate.forEach(([key, value]) => {
-            switch (key) {
-                case "name": {
-                    const updatedNameToApplyOrNot = AdvisorFactory.updateName(value, advisorExisting);
-                    if (updatedNameToApplyOrNot.isLeft()) {
-                        return updatedNameToApplyOrNot.value;
-                    }
-                    break;
-                }
-                case "surname": {
-                    const updatedSurnameToApplyOrNot = AdvisorFactory.updateSurname(value, advisorExisting);
-                    if (updatedSurnameToApplyOrNot.isLeft()) {
-                        return updatedSurnameToApplyOrNot.value;
-                    }
-                    break;
-                }
-                case "registrationNumber": {
-                    const updatedRegistrationNumberToApplyOrNot = AdvisorFactory.updateRegistrationNumber(value, advisorExisting);
-                    if (updatedRegistrationNumberToApplyOrNot.isLeft()) {
-                        return updatedRegistrationNumberToApplyOrNot.value;
-                    }
-                    break;
-
-                }
+            if (fieldUpdate.isLeft()) {
+                return fieldUpdate.value; // Retorna o erro imediatamente, se houver.
             }
-        });
 
-
-        // Irá verificar se pelos um dos campos não é undefined
-        // const hasAtLeastOneFieldDefined = Object.values(updateFields).some((field) => field !== undefined);
-        // if (!hasAtLeastOneFieldDefined) {
-        //     return new DomainError(
-        //         ErrorCategory.Application,    
-        //         "Error to update advisor attributes",
-        //         ["Any of the 3 fields must be provided."]
-        //     );
-        // }
-
-
-        // filtra os valores que não são undfined, deixando apenas o que será atualizado
-        // const advisorDefinedFieldsToUpdate = Object.entries(updateFields)
-        //     .filter(
-        //         ([key, value]) => value !== undefined) as [keyof updateAdvisorPropsDTO["updateFields"], string][]; // Garante que as chaves correspondem ao tipo UpdateFields
-        // advisorDefinedFieldsToUpdate
-        //     .forEach(([key, value]) => {
-        //         switch (key) {
-        //             case "name": {
-        //                 const updatedNameToApplyOrNot = AdvisorFactory.updateName(value, advisorExisting);
-        //                 if (updatedNameToApplyOrNot.isLeft()) {
-        //                     return updatedNameToApplyOrNot.value;
-        //                 }
-
-        //                 break;
-        //             }
-        //             case "surname": {
-        //                 const updatedSurnameToApplyOrNot = AdvisorFactory.updateSurname(value, advisorExisting);
-        //                 if (updatedSurnameToApplyOrNot.isLeft()) {
-        //                     return updatedSurnameToApplyOrNot.value;
-        //                 }
-        //                 break;
-        //             }
-        //             case "registrationNumber": {
-        //                 const updatedRegistrationNumberToApplyOrNot = AdvisorFactory.updateRegistrationNumber(value, advisorExisting);
-        //                 if (updatedRegistrationNumberToApplyOrNot.isLeft()) {
-        //                     return updatedRegistrationNumberToApplyOrNot.value;
-        //                 }
-        //                 break;
-
-        //             }
-        //             default:
-        //                 // Handle any unexpected keys or log a warning
-        //                 console.warn(`Unexpected field: ${key}`);
-        //                 break;
-        //         }
-        //     });
-        const advisor = await this.advisorRepository.updateAdvisor({}, advisorExisting.id);
-
+            validFields = { ...validFields, ...fieldUpdate.value };
+        }
+        const advisor = await this.advisorRepository.updateAdvisor(validFields, updateAdvisorProps.advisorIdentification);
         return advisor;
+        // const advisorDefinedFieldsToUpdate: UpdateFieldsDTO = Object.fromEntries(advisorDefinedFields);
+
+
+        // advisorDefinedFieldsToUpdate.
+
+        // console.log(validFields);
+        // const advisor = await this.advisorRepository.updateAdvisor(validFields, advisorExisting.id);
+
     }
+
+    private updateValidateFields(key: string, value: string): Either<DomainError, Partial<UpdateFieldsDTO>> {
+
+        switch (key) {
+            case "name": {
+                const updatedNameToApplyOrNot = AdvisorFactory.validateNameField(value);
+                if (updatedNameToApplyOrNot.isLeft()) {
+                    return new Left(updatedNameToApplyOrNot.value);
+                } else if (updatedNameToApplyOrNot.isRight())
+                    return new Right({ name: updatedNameToApplyOrNot.value });
+            }
+            case "surname": {
+                const updatedSurnameToApplyOrNot = AdvisorFactory.validateSurnameField(value);
+                if (updatedSurnameToApplyOrNot.isLeft()) {
+                    return new Left(updatedSurnameToApplyOrNot.value);
+                } if ((updatedSurnameToApplyOrNot.isRight()))
+                    return new Right({ surname: updatedSurnameToApplyOrNot.value });
+            }
+            case "registrationNumber": {
+                const updatedRegistrationNumberToApplyOrNot = AdvisorFactory.validateRegistrationNumberField(value);
+                if (updatedRegistrationNumberToApplyOrNot.isLeft()) {
+                    return new Left(updatedRegistrationNumberToApplyOrNot.value);
+                } if (updatedRegistrationNumberToApplyOrNot.isRight())
+                    return new Right({ registrationNumber: updatedRegistrationNumberToApplyOrNot.value });
+            }
+        }
+        return new Left(new DomainError(ErrorCategory.Application, "Unexpected Error", ["Undexpected error to validate fields to update "]));
+    }
+
+    // Descontinuado
+    // private updateRequiredFields(key: string, value: string, advisor: Advisor): DomainError | Partial<UpdateFieldsDTO> {
+
+    //     switch (key) {
+    //         case "name": {
+    //             const updatedNameToApplyOrNot = AdvisorFactory.updateName(value, advisor);
+    //             if (updatedNameToApplyOrNot.isLeft()) {
+    //                 return updatedNameToApplyOrNot.value;
+    //             }
+    //             return { name: advisor.name };
+    //         }
+    //         case "surname": {
+    //             const updatedSurnameToApplyOrNot = AdvisorFactory.updateSurname(value, advisor);
+    //             if (updatedSurnameToApplyOrNot.isLeft()) {
+    //                 return updatedSurnameToApplyOrNot.value;
+    //             }
+    //             return { surname: advisor.surname };
+    //         }
+    //         case "registrationNumber": {
+    //             const updatedRegistrationNumberToApplyOrNot = AdvisorFactory.updateRegistrationNumber(value, advisor);
+    //             if (updatedRegistrationNumberToApplyOrNot.isLeft()) {
+    //                 return updatedRegistrationNumberToApplyOrNot.value;
+    //             }
+    //             return { registrationNumber: advisor.registrationNumber };
+    //         }
+    //     }
+    //     return {}
+    // }
 }
--- repositiff-backEnd/src/infra/repositories/IAdvisorRepository.ts
+++ repositiff-backEnd/src/infra/repositories/IAdvisorRepository.ts
@@ -1,10 +1,11 @@
-import { updateAdvisorPropsDTO, UpdateFieldsDTO } from "@src/application/advisor-useCases/updateAdvisor.js";
+import { UpdateFieldsDTO } from "@src/application/advisor-useCases/updateAdvisor.js";
 import { Advisor } from "@src/domain/advisor.js";
 export interface IAdvisorRepository {
     cadastrationNewAdvisor(advisor: Advisor): Promise<Advisor>;
     deleteAdvisor(advisor: Advisor): Promise<void>;
     listAllAdvisors(): Promise<Advisor[]>;
-    updateAdvisor(updateAdvisorFields: UpdateFieldsDTO, id: string): Promise<Advisor>;
+    updateAdvisor(updateFields: UpdateFieldsDTO, id: string): Promise<Advisor>;
     findAdvisorById(id: String): Promise<Advisor | void>;
+    advisorExisting(id: String): Promise<boolean>;
     findAdvisorByRegistrationNumber(registrationNumber: string): Promise<void | Advisor>;
 }
